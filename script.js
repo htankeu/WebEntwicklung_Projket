53db4a77df15d7c6f799193ec33b422ee650318e
Heda Tankeu <hedayvan@gmail.com>
2023-05-29 02:01:10 +0200

    style und Javascript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Javascript Teil unserer Aufgabe
+
+
+// operationen um die MenuListe anzuzeigen und zu closeMenu, beim kleinen Dimensionen
+const sidemenu = document.getElementById("sideMenu")
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+}
