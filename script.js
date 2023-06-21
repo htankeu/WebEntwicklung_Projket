a14c81d6327e995ecacf52c8c06db877041ac83e
nenkam11111 <nenkamc@gmail.com>
2023-06-21 13:03:03 +0200

    new Kontakt seite and login page

--- script.js
+++ script.js
@@ -80,3 +80,95 @@ function istChild(parent, child){
     }
     return false
 }
+
+
+function removeAllChild(section){
+    // remove all  existed child
+    rChild = section.lastElementChild
+
+    while(rChild){
+        section.removeChild(rChild)
+        rChild = section.lastElementChild
+    }
+}
+
+
+
+
+
+
+// read a JSON file to have a information of Kits elements
+const kits = document.getElementsByTagName("figure")
+const navColors = document.querySelector('.colors')
+const colorsToChoose = navColors.getElementsByTagName("li")
+const seeElem = document.querySelector('.seeElems')
+
+const kitsTop = document.querySelector('#ktop')
+const kitsShort = document.querySelector('#kshort')
+const kitsSockets = document.querySelector('#kscokets')
+
+
+let kitChoosed = ""
+let colorChoosed = ""
+
+
+for(kit of kits){
+    for(child of kit.childNodes){ 
+        child.addEventListener('click',(e)=>{
+            removeAllChild(seeElem)
+
+            kitChoosed = e.target.id
+
+            for(color of colorsToChoose){
+
+                color.addEventListener('click',(f)=>{
+                    removeAllChild(seeElem)
+
+                    colorChoosed = f.target.id
+                    chooseKit(kitChoosed,colorChoosed)
+
+                })
+            }
+        })
+    }
+}
+
+
+function chooseKit(kitToChoose,color){
+    fetch("data.json").then(resolve => resolve.json())
+.then(data => data.forEach(element =>{
+        
+    if(element['part'] == kitToChoose && element['color'] == color){
+         var elemDiv = document.createElement('div')
+         elemDiv.classList.add('imageOfElem')
+         var elemImg = document.createElement('img')
+         elemImg.src = element['url']
+         elemDiv.appendChild(elemImg)
+         seeElem.appendChild(elemDiv)
+
+         elemImg.addEventListener('click',(e)=>{
+            if(kitToChoose == 'top') kitsTop.src = e.target.src
+            else if(kitToChoose == 'short') kitsShort.src = e.target.src
+            else if(kitToChoose == 'socket') kitsSockets.src = e.target.src
+         })
+    } 
+    
+}))
+}
+
+//Login Page
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+  
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Vérification des informations de connexion
+    if (username === 'utilisateur' && password === 'motdepasse') {
+      alert('Connexion réussie !'); // Remplacez par votre propre logique de connexion
+    } else {
+      alert('Identifiant ou mot de passe incorrect !'); // Remplacez par votre propre logique de connexion
+    }
+});
+
+
